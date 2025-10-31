003575863b687ab69568352501b39ff3fd47510c
GUILLERMO RECALDE <recaldev.ga@gmail.com>
2025-10-30 22:38:22 -0300

    Actualizacion de funciones

--- fix_not_null_constraints.sql
+++
@@ -1,6 +0,0 @@
--- Script para permitir NULL en nombre y apellido (temporal, hasta eliminar columnas)
--- Ejecutar este script para solucionar el error de NOT NULL
-
-ALTER TABLE postulantes ALTER COLUMN nombre DROP NOT NULL;
-ALTER TABLE postulantes ALTER COLUMN apellido DROP NOT NULL;
-
